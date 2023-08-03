cab9cf7ac6403e00e9315b286667a708a913e966
unknown <santhoshbca9384@gmail.com>
2023-08-03 15:31:42 +0530

    new

---
+++ script/eyeopen.js
@@ -0,0 +1,13 @@
+function trunOff(){
+    document.getElementById('myImage').src='./image/eye  close.png'
+    }
+    function trunOn(){
+       document.getElementById('myImage').src='./image/eye  open.png'
+    }
+    function hide() {
+      document.getElementById('demo').style.display='none'
+    }
+function show()  {
+   document.getElementById('show').style.display='block'
+   }
+  
